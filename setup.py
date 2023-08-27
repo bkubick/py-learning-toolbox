1972d46015d6862b1b41173e379a7f4c88f80423
bkubick <bjk2158@columbia.edu>
2023-08-27 11:11:28 -0500

    Add setup.py file (WIP)

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='py-learning-toolbox',
+    version='0.0.1',
+    description='Python toolbox for my perosnal AI, machine learning, genetic algorithms, and deep learning projects.',
+    author='Brandon Kubick',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib>=3.7.2',
+        'numpy>=1.24.3',
+        'pandas>=2.0.3',
+        'tensorboard>=2.13.0',
+        'tensorflow>=2.13.0',
+        'tensorflow-hub>=0.14.0',
+        'scikit-learn>=1.3.0',
+    ],
+)
