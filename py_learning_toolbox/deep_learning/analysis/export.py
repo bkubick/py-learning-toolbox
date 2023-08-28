57935d8b9d206a28d785ab94ef8c42b4edecf2dc
bkubick <bjk2158@columbia.edu>
2023-08-28 08:26:47 -0500

    Add timestamp arg for filename and fix typehchecking on export

--- py_learning_toolbox/deep_learning/analysis/export.py
+++ py_learning_toolbox/deep_learning/analysis/export.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 
-from __future__ import absolute_import
+from __future__ import annotations
 
+import datetime as dt
 import io
 import typing
 
-import tensorflow as tf
+if typing.TYPE_CHECKING:
+    import tensorflow as tf
 
 
 __all__ = ['export_embedding_projector_data']
@@ -13,7 +15,8 @@ __all__ = ['export_embedding_projector_data']
 
 def export_embedding_projector_data(embedded_layer_weights: tf.Tensor,
                                     vocabulary: typing.List[str],
-                                    filepath: typing.Optional[str] = None):
+                                    filepath: typing.Optional[str] = None,
+                                    include_timestamp: bool = False):
     """ Exports the embedding projector data to filenames listed below.
         - vectors.tsv : The embedding vectors.
         - metadata.tsv : The words in the vocabulary.
@@ -27,9 +30,13 @@ def export_embedding_projector_data(embedded_layer_weights: tf.Tensor,
             embedded_layer_weights (tf.Tensor): The weights of the embedded layer.
             vocabulary (typing.List[str]): The words in the vocabulary.
             filepath (str): The filepath to save the data to.
+            include_timestamp (bool): Whether to include a timestamp in the filepath.
     """
     # Create embedding files (These will be uploaded to the embedding projector)
     embedding_data_filepath = filepath or '.'
+
+    if include_timestamp:
+        embedding_data_filepath = f'{embedding_data_filepath}/{dt.datetime.now().strftime("%Y%m%d-%H%M%S")}'
 
     out_v = io.open(f'{embedding_data_filepath}/vectors.tsv', 'w', encoding='utf-8')
     out_m = io.open(f'{embedding_data_filepath}/metadata.tsv', 'w', encoding='utf-8')
