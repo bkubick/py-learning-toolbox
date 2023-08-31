87ee153b2adfe1bb89368310d126b2f6cf6a8fc3
bkubick <bjk2158@columbia.edu>
2023-08-31 16:34:40 -0500

    Remove dataset module
    The dataset module was experimental, and after digging into
    the TensorFlow docs, this module is not needed.

--- py_learning_toolbox/data_toolbox/__init__.py
+++ py_learning_toolbox/data_toolbox/__init__.py
@@ -1,17 +1,12 @@
 # coding: utf-8
-""" This package contains all the utilities for working with data. This includes functions for loading data and
-    structuring datasets.
+""" This package contains all the utilities for working with data. This includes functions for loading and
+    structuring data.
 """
 
-from .dataset import Dataset, get_dataset_from_data, get_dataset_from_train_test_val, get_split_dataset_from_data
 from .utils import read_txt_file_from_directory, read_txt_file_from_url
 
 
 __all__ = [
-    'Dataset',
-    'get_dataset_from_data',
-    'get_dataset_from_train_test_val',
-    'get_split_dataset_from_data',
     'read_txt_file_from_directory',
     'read_txt_file_from_url',
 ]
