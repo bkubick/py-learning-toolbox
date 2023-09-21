d1ef54d2538aa38c38d2801ea7067916e8c0f977
bkubick <bjk2158@columbia.edu>
2023-09-21 12:25:35 -0500

    Update embedding project data file stored location

--- py_learning_toolbox/ml_toolbox/analysis/language.py
+++ py_learning_toolbox/ml_toolbox/analysis/language.py
@@ -14,7 +14,8 @@ if typing.TYPE_CHECKING:
 __all__ = ['export_embedding_projector_data']
 
 
-def export_embedding_projector_data(embedded_layer_weights: tf.Tensor,
+def export_embedding_projector_data(model_name: str,
+                                    embedded_layer_weights: tf.Tensor,
                                     vocabulary: typing.List[str],
                                     filepath: typing.Optional[str] = None,
                                     include_timestamp: bool = False):
@@ -22,19 +23,24 @@ def export_embedding_projector_data(embedded_layer_weights: tf.Tensor,
         - vectors.tsv : The embedding vectors.
         - metadata.tsv : The words in the vocabulary.
 
+        Files are stored at:
+            {filepath}/{model_name}/embedding_projector_data/{timestamp}
+
         This code, as well as the embedding projector, is based on the following resources:
         - https://www.tensorflow.org/text/guide/word_embeddings : See the section on "Retrieving the trained
           embeddings and their metadata".
         - https://projector.tensorflow.org/ : The embedding projector.
 
         Args:
+            model_name (str): The name of the model.
             embedded_layer_weights (tf.Tensor): The weights of the embedded layer.
             vocabulary (typing.List[str]): The words in the vocabulary.
             filepath (str): The filepath to save the data to.
+                Defaults to 'logs'
             include_timestamp (bool): Whether to include a timestamp in the filepath.
     """
     # Create embedding files (These will be uploaded to the embedding projector)
-    embedding_data_filepath = filepath or '.'
+    embedding_data_filepath = f'{filepath or "logs"}/{model_name}/embedding_projector_data'
 
     if include_timestamp:
         embedding_data_filepath = f'{embedding_data_filepath}/{dt.datetime.now().strftime("%Y%m%d-%H%M%S")}'
