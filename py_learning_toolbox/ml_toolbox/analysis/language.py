b4eecc3089bc10fe6e84d75d725c8494361645a6
bkubick <bjk2158@columbia.edu>
2023-09-29 19:53:38 -0500

    Add language analysis utilities to get and plot word counts

--- py_learning_toolbox/ml_toolbox/analysis/language.py
+++ py_learning_toolbox/ml_toolbox/analysis/language.py
@@ -7,6 +7,9 @@ import io
 import os
 import typing
 
+import matplotlib.pyplot as plt
+
+
 if typing.TYPE_CHECKING:
     import tensorflow as tf
 
@@ -61,3 +64,54 @@ def export_embedding_projector_data(model_name: str,
 
     out_v.close()
     out_m.close()
+
+
+def get_words_by_count(sentences: typing.List[str]) -> typing.Dict[str, int]:
+    """ Gets the words by count in all the sentences.
+            
+        Args:
+            sentences (List[str]): The sentences to get the words by count from.
+
+        Returns:
+            (Dict[str, int]) The words by count.
+    """
+    words_by_count = {}
+    for sentence in sentences:
+        if len(sentence) == 1:
+            continue
+
+        for word in sentence.split(' '):
+            words_by_count[word] = words_by_count.get(word, 0) + 1
+
+    return words_by_count
+
+
+def plot_words_by_count(words_by_count: typing.Dict[str, int],
+                        n: int = 20,
+                        most_common: bool = True,
+                        figsize: typing.Tuple[int, int] = (8, 5),
+                        tick_fontsize: int = 8) -> None:
+    """ Plots the n most or least common words based on the counts.
+
+        Args:
+            words_by_count (Dict[str, int]): the word count by each word (i.e. [{'obi-wan': 4}... ]).
+                NOTE: a complimentary function to generate this is `get_words_by_count`.
+            n (int): the number of words to include in plot.
+            most_common (bool): whether to plot the most or least common words.
+            tick_fontsize (int): the fontsize for the x and y ticks.
+    """
+    sorted_words = sorted(words_by_count, key=lambda x: words_by_count[x])
+    counts = [words_by_count[word] for word in sorted_words]
+
+    n_words = sorted_words[-n:] if most_common else sorted_words[:n]
+    n_counts = counts[-n:] if most_common else counts[:n]
+
+    title = f'Top {n} {"Most" if most_common else "Least"} Commonly Used Words'
+
+    plt.figure(figsize=figsize)
+    plt.bar(n_words, n_counts)
+    plt.xticks(rotation=45, fontsize=tick_fontsize)
+    plt.yticks(rotation=0, fontsize=tick_fontsize)
+    plt.xlabel('Most Common Words:', fontsize=12)
+    plt.ylabel('Number of Occurences:', fontsize=12)
+    plt.title(title, fontsize=12)
