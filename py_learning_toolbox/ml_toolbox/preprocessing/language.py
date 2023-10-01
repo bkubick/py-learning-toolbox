2ffdf0a2d3a2ad3375f7b739a02d9af2867d6d5b
bkubick <bjk2158@columbia.edu>
2023-10-01 15:16:50 -0500

    Add additional language preprocessing utilities for creating data

--- py_learning_toolbox/ml_toolbox/preprocessing/language.py
+++ py_learning_toolbox/ml_toolbox/preprocessing/language.py
@@ -4,16 +4,23 @@ from __future__ import annotations
 
 import re
 import string
+import typing
+
+import tensorflow as tf
 
 
-__all__ = ['clean_text']
+__all__ = [
+    'clean_text',
+    'create_sequences_from_sentences',
+    'create_sequenced_data_labels_from_sentences',
+]
 
 
 def clean_text(text: str,
-                    strip_punctuation: bool = True,
-                    lowercase: bool = True,
-                    strip_leading_and_trailing_whitespace: bool = True,
-                    strip_spacing_characters: bool = True) -> str:
+               strip_punctuation: bool = True,
+               lowercase: bool = True,
+               strip_leading_and_trailing_whitespace: bool = True,
+               strip_spacing_characters: bool = True) -> str:
     """ Cleans the given text.
 
         Args:
@@ -41,3 +48,49 @@ def clean_text(text: str,
         text = re.sub(r'\s+', ' ', text)
 
     return text
+
+
+def create_sequences_from_sentences(sentences: typing.List[str]) -> tf.Tensor:
+    """ Creates sequences of sentences from the given sentences.
+
+        Example:
+            Input: ['I love TensorFlow and AI']
+            Output: ['I love', 'I love TensorFlow', 'I love TensorFlow and', 'I love TensorFlow and AI']
+
+        Args:
+            sentences (List[str]): The sentences to create sequences from.
+
+        Returns:
+            (tf.Tensor) The sequences of sentences.
+    """
+    sequences = []
+    for sentence in sentences:
+        words_in_sentence = sentence.split(' ')
+        for i in range(2, len(words_in_sentence) + 1):
+            sequences.append(' '.join(words_in_sentence[:i]))
+
+    return tf.convert_to_tensor(sequences, dtype=tf.string)
+
+
+def create_sequenced_data_labels_from_sentences(sentences: typing.List[str]) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """ Generates sequenced data from the given sentences and returns the data and labels.
+
+        Example:
+            Input: ['I love TensorFlow and AI']
+            Output: (['I love', 'I love TensorFlow', 'I love TensorFlow and'],
+                    ['TensorFlow', 'and', 'AI'])
+
+        Args:
+            sentences (List[str]): The sentences to create sequences from.
+
+        Returns:
+            (Tuple[tf.Tensor, tf.Tensor]) The sequenced data.
+    """
+    data, labels = [], []
+    for sentence in sentences:
+        words_in_sentence = sentence.split(' ')
+        for i in range(1, len(words_in_sentence)):
+            data.append(' '.join(words_in_sentence[:i]))
+            labels.append(words_in_sentence[i])
+
+    return tf.convert_to_tensor(data, dtype=tf.string), tf.convert_to_tensor(labels, dtype=tf.string)
