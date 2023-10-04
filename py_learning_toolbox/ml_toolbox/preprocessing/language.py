4c0ef88ddfda08f5eea5f130255ba3fd86f40742
bkubick <bjk2158@columbia.edu>
2023-10-04 18:09:06 -0500

    Make the text sequencer utils work with character level sequencing

--- py_learning_toolbox/ml_toolbox/preprocessing/language.py
+++ py_learning_toolbox/ml_toolbox/preprocessing/language.py
@@ -38,42 +38,57 @@ def clean_text(text: str,
 
     if strip_punctuation:
         text = text.translate(str.maketrans('', '', string.punctuation))
-    
+
     if lowercase:
         text = text.lower()
-    
+
     if strip_leading_and_trailing_whitespace:
         text = text.strip()
-    
+
     if strip_spacing_characters:
         text = re.sub(r'\s+', ' ', text)
 
     return text
 
 
-def create_sequences_from_sentences(sentences: typing.List[str]) -> typing.List[str]:
+def create_sequences_from_sentences(sentences: typing.List[str],
+                                    level: typing.Optional[str] = None) -> typing.List[str]:
     """ Creates sequences of sentences from the given sentences.
 
         Example:
             Input: ['I love TensorFlow and AI']
             Output: ['I love', 'I love TensorFlow', 'I love TensorFlow and', 'I love TensorFlow and AI']
 
+        Raises:
+            ValueError: If the level is not one of ['word', 'character'].
+
         Args:
             sentences (List[str]): The sentences to create sequences from.
+            level (Optional[str]): The level to create sequences from.
+                Must be one of ['word', 'character'], defaults to 'word'.
 
         Returns:
             (typing.List[str]) The sequences of sentences.
     """
+    level = level or 'word'
+
     sequences = []
     for sentence in sentences:
-        words_in_sentence = sentence.split(' ')
-        for i in range(2, len(words_in_sentence) + 1):
-            sequences.append(' '.join(words_in_sentence[:i]))
+        if level == 'word':
+            words_in_sentence = sentence.split(' ')
+            for i in range(2, len(words_in_sentence) + 1):
+                sequences.append(' '.join(words_in_sentence[:i]))
+        elif level == 'character':
+            for i in range(2, len(sentence) + 1):
+                sequences.append(sentence[:i])
+        else:
+            raise ValueError(f'level must be one of ["word", "character"], got {level}')
 
     return sequences
 
 
-def create_sequenced_data_labels_from_sentences(sentences: typing.List[str]) -> DataLabelTuple:
+def create_sequenced_data_labels_from_sentences(sentences: typing.List[str],
+                                                level: typing.Optional[str] = None) -> DataLabelTuple:
     """ Generates sequenced data from the given sentences and returns the data and labels.
 
         Example:
@@ -81,17 +96,31 @@ def create_sequenced_data_labels_from_sentences(sentences: typing.List[str]) -> 
             Output: (['I love', 'I love TensorFlow', 'I love TensorFlow and'],
                     ['TensorFlow', 'and', 'AI'])
 
+        Raises:
+            ValueError: If the level is not one of ['word', 'character'].
+
         Args:
             sentences (List[str]): The sentences to create sequences from.
+            level (Optional[str]): The level to create sequences from.
+                Must be one of ['word', 'character'], defaults to 'word'.
 
         Returns:
             (Tuple[typing.List[str], typing.List[str]]) The sequenced data and labels.
     """
+    level = level or 'word'
+
     data, labels = [], []
     for sentence in sentences:
-        words_in_sentence = sentence.split(' ')
-        for i in range(1, len(words_in_sentence)):
-            data.append(' '.join(words_in_sentence[:i]))
-            labels.append(words_in_sentence[i])
+        if level == 'word':
+            words_in_sentence = sentence.split(' ')
+            for i in range(1, len(words_in_sentence)):
+                data.append(' '.join(words_in_sentence[:i]))
+                labels.append(words_in_sentence[i])
+        elif level == 'character':
+            for i in range(1, len(sentence)):
+                data.append(sentence[:i])
+                labels.append(sentence[i])
+        else:
+            raise ValueError(f'level must be one of ["word", "character"], got {level}')
 
     return data, labels
